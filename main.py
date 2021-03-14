8dea68f9664e32ced091350509473a694bb2aa3e
kool8 <57043683+kool-8@users.noreply.github.com>
2021-03-15 03:12:05 +0900

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,6 @@ from pprint import pp
 w = Wallet()
 
 start = time()
-a = w("0xb732a5B907FC38eD71A9B18eBd77dDa3F2771103").yields_with_history()
+a = w('').yields_with_history()
 pp(a)
 print(time()-start, 'seconds')
